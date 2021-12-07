f5c431abeb73fbf3a6f409fedbccec89112bd377
GeneTv <contact@genetv.net>
2021-12-07 11:12:35 +0100

    Finally done

--- app/src/main/java/io/genetv/counterapp/MainActivity.java
+++ app/src/main/java/io/genetv/counterapp/MainActivity.java
@@ -2,6 +2,8 @@ package io.genetv.counterapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Button btnIncrement;
     private Button btnDecrement;
+    private Button btnReset;
     private TextView displayText;
 
     private int currentCount = 0;
@@ -22,25 +25,49 @@ public class MainActivity extends AppCompatActivity {
 
         btnIncrement = findViewById(R.id.btnIncrement);
         btnDecrement = findViewById(R.id.btnDecrement);
-        displayText = findViewById(R.id.displayText);
+        btnReset = findViewById(R.id.btnReset);
+        displayText = findViewById(R.id.displayCurrentCount);
 
         btnIncrement.setOnClickListener(v -> {
             this.currentCount++;
             updateView(currentCount);
-            //Toast.makeText(this, "Added 1", Toast.LENGTH_SHORT).show();
         });
 
         btnDecrement.setOnClickListener(v -> {
             if(currentCount == 0) return;
             this.currentCount--;
             updateView(currentCount);
-            //Toast.makeText(this, "Removed 1", Toast.LENGTH_SHORT).show();
         });
 
-        updateView(currentCount);
+        btnReset.setOnClickListener(v -> {
+            this.currentCount = 0;
+            updateView(currentCount);
+            Toast.makeText(this, R.string.btn_message_reset, Toast.LENGTH_SHORT).show();
+        });
+
+        SharedPreferences counterPreference = getSharedPreferences("counter", Context.MODE_PRIVATE);
+        currentCount = counterPreference.getInt("last_count", 0);
+
+        updateView(currentCount, false);
     }
 
-    private void updateView(int newCount) {
+
+    private void updateView(int newCount) { updateView(newCount, true); }
+    private void updateView(int newCount, boolean save) {
+        if(save) {
+            SharedPreferences.Editor sharedPref = getSharedPreferences("counter", Context.MODE_PRIVATE).edit();
+            sharedPref.putInt("last_count", newCount);
+            sharedPref.apply();
+        }
+
         displayText.setText(String.valueOf(newCount));
+
+        if(currentCount == 0) {
+            btnDecrement.setEnabled(false);
+            btnReset.setEnabled(false);
+        } else if (!btnDecrement.isEnabled() || !btnReset.isEnabled()){
+            btnDecrement.setEnabled(true);
+            btnReset.setEnabled(true);
+        }
     }
 }
