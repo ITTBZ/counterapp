de14fba45596defb4fd9274307f0a6ebc13604c2
GeneTv <contact@genetv.net>
2021-12-14 13:13:50 +0100

    Some changes

--- app/src/main/java/io/genetv/counterapp/MainActivity.java
+++ app/src/main/java/io/genetv/counterapp/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -39,10 +42,20 @@ public class MainActivity extends AppCompatActivity {
             updateView(currentCount);
         });
 
-        btnReset.setOnClickListener(v -> {
+        btnReset.setOnClickListener(btn -> {
             this.currentCount = 0;
             updateView(currentCount);
             Toast.makeText(this, R.string.btn_message_reset, Toast.LENGTH_SHORT).show();
+
+            Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            // Vibrate for 500 milliseconds
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                v.vibrate(VibrationEffect.createOneShot(200, VibrationEffect.DEFAULT_AMPLITUDE));
+            } else {
+                //deprecated in API 26
+                v.vibrate(200);
+            }
+
         });
 
         SharedPreferences counterPreference = getSharedPreferences("counter", Context.MODE_PRIVATE);
